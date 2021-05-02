26457a6843bf369c2944e3e44177fbeda69ccf21
sanggu <tkdrn8578@naver.com>
2021-05-02 23:04:56 +0900

    feat: List<Records> 를 OverallRecords 일급컬렉션으로 추출

---
+++ src/main/java/racingcar/domain/OverallRecords.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OverallRecords {
+
+    private final List<Records> overallRecords;
+
+    public OverallRecords(List<Records> overallRecords) {
+        this.overallRecords = overallRecords;
+    }
+
+    public OverallRecords() {
+        this.overallRecords = new ArrayList<>();
+    }
+
+    public OverallRecords add(Records records) {
+        overallRecords.add(records);
+        return new OverallRecords(overallRecords);
+    }
+
+    public Records get(int index) {
+        return overallRecords.get(index);
+    }
+
+    public int size() {
+        return overallRecords.size();
+    }
+}
