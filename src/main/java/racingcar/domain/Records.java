d610e49c9d583522ef76971d4f58d9323765652f
sanggu <tkdrn8578@naver.com>
2021-05-02 22:52:22 +0900

    feat: Records를 일급컬렉션으로 추출

---
+++ src/main/java/racingcar/domain/Records.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Records {
+
+    private final List<Record> records;
+
+    public Records() {
+        this.records = new ArrayList<>();
+    }
+
+    public Records(List<Record> records) {
+        this.records = records;
+    }
+
+    public Records add(Record record) {
+        records.add(record);
+        return new Records(records);
+    }
+
+    public Record get(int index) {
+        return records.get(index);
+    }
+
+    public int size() {
+        return records.size();
+    }
+}
