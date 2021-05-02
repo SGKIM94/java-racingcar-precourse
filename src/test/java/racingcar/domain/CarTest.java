a1b758677f51140c1e6f36beaa1b7a93fa0f98fd
sanggu <tkdrn8578@naver.com>
2021-05-02 16:15:20 +0900

    refactor: ParameterizedTest를 사용하여 테스트의 중복 제거

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,28 +1,20 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @Test
-    @DisplayName("자동차를 전진시키면 위치가 한칸 증가한다.")
-    void forward() {
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("4 이상 9이하인 값이면 전진한다.")
+    void forwardByCondition(int condition) {
         Car car = new Car("kim");
 
-        car.forward();
-
-        assertThat(car.getPosition()).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("4 이상의의 값이면 전진한다.")
-    void forwardByCondition() {
-        Car car = new Car("kim");
-
-        car.forwardByCondition(10);
+        car.forwardByCondition(condition);
 
         assertThat(car.getPosition()).isEqualTo(1);
     }
