a374b55f81846868e7e42618ae61894780b503fe
sanggu <tkdrn8578@naver.com>
2021-05-02 20:32:19 +0900

    feat: 자동차 경주의 기록을 관리하기 위한 Record 클래스 추가

---
+++ src/main/java/racingcar/domain/Record.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class Record {
+
+    private CarName name;
+    private Position position;
+
+    public Record(CarName name, Position position) {
+        this.name = name;
+        this.position = position;
+    }
+}
