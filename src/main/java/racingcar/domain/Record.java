66470cc0c06dcf099d407184e1a86e290b76639e
sanggu <tkdrn8578@naver.com>
2021-05-02 22:32:37 +0900

    feat: 동등성을 보장하기 위한 equals와 hashcode추가

--- src/main/java/racingcar/domain/Record.java
+++ src/main/java/racingcar/domain/Record.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Record {
 
     private CarName name;
@@ -8,5 +10,18 @@ public class Record {
     public Record(CarName name, Position position) {
         this.name = name;
         this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Record record = (Record) o;
+        return Objects.equals(name, record.name) && Objects.equals(position, record.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
