7216fdf746218f80fbc637699a41fbec3106af38
sanggu <tkdrn8578@naver.com>
2021-05-02 16:22:44 +0900

    refactor: Position으로 변경함에 따른 Car 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,25 +5,21 @@ public class Car {
     public static final int START_POSITION = 0;
     public static final int MOVABLE_CONDITION = 4;
 
-    private int position;
+    private Position position;
     private String name;
 
     public Car(String name) {
         this.name = name;
-        this.position = START_POSITION;
+        this.position = new Position(START_POSITION);
     }
 
     public void forwardByCondition(int condition) {
         if (condition >= MOVABLE_CONDITION) {
-            forward();
+            position.forward();
         }
     }
 
-    private void forward() {
-        this.position++;
-    }
-
     public int getPosition() {
-        return position;
+        return position.getPosition();
     }
 }
