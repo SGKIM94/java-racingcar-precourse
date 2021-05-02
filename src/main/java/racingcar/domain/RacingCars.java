8e6a46283b2722bf3646d97af5065ec6f77287cf
sanggu <tkdrn8578@naver.com>
2021-05-02 20:53:06 +0900

    refactor: Cars 클래스 추출에 따른 로직 수정

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars {
-    private List<Car> cars;
-    private List<Integer> conditions;
+
+    private final Cars cars;
+    private final List<Integer> conditions;
 
     public RacingCars(List<Car> cars, List<Integer> conditions) {
-        this.cars = cars;
+        this.cars = new Cars(cars);
         this.conditions = conditions;
     }
 
@@ -20,29 +21,10 @@ public class RacingCars {
         List<List<Record>> allRoundRecords = new ArrayList<>();
 
         for (int round = 0; round < rounds; round++) {
-            forwardAllByConditions();
-            allRoundRecords.add(recordAllRound());
+            cars.forwardAllByConditions(conditions);
+            allRoundRecords.add(cars.recordAllRound());
         }
 
         return allRoundRecords;
-    }
-
-    private List<Record> recordAllRound() {
-        List<Record> records = new ArrayList<>();
-
-        for (Car car : cars) {
-            records.add(new Record(car.getName(), car.getPosition()));
-        }
-
-        return records;
-    }
-
-    private void forwardAllByConditions() {
-        int sizeOfCars = cars.size();
-
-        for (int index = 0; index < sizeOfCars; index++) {
-            Car currentCar = cars.get(index);
-            currentCar.forwardByCondition(conditions.get(index));
-        }
     }
 }
