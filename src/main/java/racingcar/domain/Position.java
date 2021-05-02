2b6d27f6354eff3c35cdad0c7d13c9b98546637a
sanggu <tkdrn8578@naver.com>
2021-05-02 16:43:00 +0900

    refactor: 불필요한 getter 제거

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -14,10 +14,6 @@ public class Position {
         this.position++;
     }
 
-    public int getPosition() {
-        return position;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
