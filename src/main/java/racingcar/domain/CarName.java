68258aae76d1276e8d243109bffd602960cb7413
sanggu <tkdrn8578@naver.com>
2021-05-02 16:48:37 +0900

    feat: 자동차의 이름에 대한 String 값을 원시값 포장한 CarName으로 추출

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class CarName {
+
+    public static final int NAME_MAX_LENGTH = 5;
+
+    private String name;
+
+    public CarName(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() >= NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("이름의 길이가 너무 깁니다. : " + name.length());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
